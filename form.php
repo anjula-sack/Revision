9601080610edce91e7039f5df928a9fd4c40bd63
anjula-sack <anjulashanaka@gmail.com>
2020-02-14 18:28:06 +0530

    Update

--- form.php
+++ form.php
@@ -8,6 +8,20 @@
 <?php
 echo $_POST["email"];
 echo $_POST["password"];
+
+$servername = "localhost";
+$username = "root";
+$password = "Root!2356";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
 ?>
 </body>
 </html>
