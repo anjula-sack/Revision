2340f68605961e83237cf178a68e68adae29bb5d
anjula-sack <anjulashanaka@gmail.com>
2020-02-14 14:09:00 +0530

    Initial Commit

---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>form</title>
+</head>
+<body>
+<?php
+echo $_POST["email"];
+echo $_POST["password"];
+?>
+</body>
+</html>
