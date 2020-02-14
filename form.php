ef7287017e96881ac1045dee53b636ff33282e20
anjula-sack <anjulashanaka@gmail.com>
2020-02-14 19:10:08 +0530

    Linked Bootstrap for form.html

--- form.php
+++ form.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
 <?php
 $email = $_GET["email"];
-$pword = $_GET["password"];
+$password = $_GET["password"];
 
 // Create connection
 $conn = new mysqli("localhost","root","Root!23456","Revision");
@@ -18,15 +20,25 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-$sql = "INSERT INTO user (username, password) VALUES ('$email', '$pword')";
+$sql = "INSERT INTO user (username, password) VALUES ('$email', '$password')";
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
 ?>
+
+<div class="container">
+    <h1>
+        <h1 class="text-center">Almost Done!</h1>
+    </h1>
+</div>
+
 </body>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </html>
